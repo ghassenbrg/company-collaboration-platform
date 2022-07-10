e49add345b37f60342e35138ec4bb68f695f663b
Mohamed Dhia Hachem <dhiua99@gmail.com>
2022-07-10 02:20:43 +0200

    load fake data

---
+++ src/main/java/tn/esprit/config/SetupDataLoader.java
@@ -0,0 +1,83 @@
+package tn.esprit.config;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.javafaker.Faker;
+
+import tn.esprit.model.Geo;
+import tn.esprit.model.user.Employee;
+import tn.esprit.model.user.Role;
+import tn.esprit.model.user.RoleName;
+import tn.esprit.model.user.UserAddress;
+import tn.esprit.repository.user.RoleRepository;
+import tn.esprit.repository.user.UserRepository;
+
+/**
+ * 
+ * @author Mohamed Dhia Hachem
+ *
+ */
+
+@Component
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+	private boolean alreadySetup = false;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public void onApplicationEvent(final ContextRefreshedEvent event) {
+		if (alreadySetup) {
+			return;
+		}
+
+		// Create initial roles
+		Role roleAdmin = createRoleIfNotFound(RoleName.ROLE_ADMIN);
+		Role roleUser = createRoleIfNotFound(RoleName.ROLE_USER);
+		Role rolePartner = createRoleIfNotFound(RoleName.ROLE_PARTNER);
+
+		// init faker
+		Faker faker = new Faker();
+
+		for (int i = 0; i < 100; i++) {
+			String username = faker.name().username();
+			Employee employee = new Employee(faker.name().firstName(), faker.name().lastName(),
+					username.length() > 15 ? username.substring(0, 14) : username, faker.internet().emailAddress(),
+					"123456");
+
+			Geo geo = new Geo(String.valueOf(faker.random().nextInt(1, 10)),
+					String.valueOf(faker.random().nextInt(1, 10)));
+			UserAddress userAddress = new UserAddress(faker.address().streetAddress(), faker.address().city(),
+					faker.address().zipCode(), geo);
+			employee.setAddress(userAddress);
+			employee.setPhone(faker.phoneNumber().phoneNumber());
+			employee.setRoles(Arrays.asList(roleUser));
+			userRepository.save(employee);
+		}
+
+		alreadySetup = true;
+	}
+
+	@Transactional
+	private final Role createRoleIfNotFound(RoleName name) {
+		Optional<Role> roleFromDb = roleRepository.findByName(name);
+		if (!roleFromDb.isPresent()) {
+			Role role = new Role(name);
+			return roleRepository.save(role);
+		}
+		return roleFromDb.get();
+	}
+
+}
